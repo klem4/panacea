139fce1f2676cfb76e6b40fa8af7aa801e8cf48c
klem4 <useperlordie@gmail.com>
2013-07-28 14:10:40 +0400

    додбавлены методы работы с request и response вреймворков

--- panacea/frameworks.py
+++ panacea/frameworks.py
@@ -1,6 +1,11 @@
 # coding: utf-8
 
 class RestFramework(object):
+    def __init__(self, request, response):
+        self.request = request
+        self.response = response
+
+
     @property
     def method(self):
         return self._get_method()
@@ -14,15 +19,30 @@ class RestFramework(object):
         return self._get_content_type()
 
 
-    def _get_method(self): raise NotImplementedError()
-    def _get_status_code(self): raise NotImplementedError()
-    def _get_content_type(self): raise NotImplementedError()
+    def _get_method(self):
+        raise NotImplementedError()
+
+    def _get_status_code(self):
+        raise NotImplementedError()
+
+    def _get_content_type(self):
+        raise NotImplementedError()
 
 
 class DjangoRestFramework0x(RestFramework):
-    pass
+    def _get_method(self):
+        return self.request.method
+
+    def _get_status_code(self):
+        return self.status_code
+
+    def _get_content_type(self):
+        # can raise IndexError, TypeError
+        # TODO: проверить
+        return self.response._headers.get('content-type')
 
 
-class DjangoRestFramework2x(RestFramework):
-    pass
-
+class DjangoRestFramework2x(DjangoRestFramework0x):
+    def _get_content_type(self):
+        # can raise attribute error
+        return self.response.accepted_media_type
