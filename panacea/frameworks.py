3f892d5e1c8b472554ad446f3713db5806f490fd
klem4 <useperlordie@gmail.com>
2013-07-28 13:44:15 +0400

    добавлена смоковые интергационные тесты на мидлвейр

---
+++ panacea/frameworks.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+class RestFramework(object):
+    @property
+    def method(self):
+        return self._get_method()
+
+    @property
+    def status_code(self):
+        return self._get_status_code()
+
+    @property
+    def content_type(self):
+        return self._get_content_type()
+
+
+    def _get_method(self): raise NotImplementedError()
+    def _get_status_code(self): raise NotImplementedError()
+    def _get_content_type(self): raise NotImplementedError()
+
+
+class DjangoRestFramework0x(RestFramework):
+    pass
+
+
+class DjangoRestFramework2x(BaseFramework):
+    pass
+
