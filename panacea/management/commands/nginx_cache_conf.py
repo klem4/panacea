ac884a0f336cf6a252f43a18850a69ff8e6e4be3
klem4 <useperlordie@gmail.com>
2013-08-17 20:12:24 +0400

    в процессе генерация конфига nginx

--- panacea/management/commands/nginx_cache_conf.py
+++ panacea/management/commands/nginx_cache_conf.py
@@ -1,11 +1,24 @@
 # coding: utf-8
 
-from django.core.management.base import BaseCommand
+import  re
+from django.core.management.base import BaseCommand, CommandError
+from django.core import urlresolvers
+from django.template.loader import render_to_string
+
 from panacea import config as conf
 from panacea.schemes import CacheScheme
 from panacea.tools import get_logger
 
 logger = get_logger()
+
+class FakeRequest(object):
+    def __init__(self, path='', query_string_dict=None,
+                 cookies_dict=None, headers_dict=None):
+        self.GET = query_string_dict or {}
+        self.COOKIES = cookies_dict or {}
+        self.META = headers_dict or {}
+        self.path = path
+
 
 class Command(BaseCommand):
     help = u"построение блока кофигурации nginx " \
@@ -22,7 +35,29 @@ class Command(BaseCommand):
         ).keys():
             scheme = CacheScheme.filter(alias=alias)
             if not scheme:
-                logger.error('No scheme for alias %s' % alias)
-                continue
+                raise CommandError('No scheme for alias %s' % alias)
 
-            
+            print self.get_location(scheme)
+
+    #api/promo/single/(?P<pk>\d+)/?$
+    def get_location(self, scheme):
+        alias = scheme.alias
+
+        urlconf = urlresolvers.get_resolver(None).reverse_dict.get(alias)
+        if not urlconf:
+            raise CommandError('No reverse match for api: %s' % alias)
+
+        pattern = urlconf[1]
+        replace_map = (
+           (r'/\(\?P<.+?>(.+?)\)/', r'/\1/'),
+            ('^/(.+)', '\1'),
+            ('\$$', '')
+        )
+
+        for repl in replace_map:
+            pattern = re.sub(repl[0], repl[1], pattern)
+
+        return "~ ^/%s$" % pattern
+
+
+
